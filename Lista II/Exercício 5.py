22eb3184d7306cb00383673d8cef6d388c3a274c
Developer-Martins <henriquemartins7t@gmail.com>
2025-09-06 19:25:55 -0300

    Terminado exercício 5 da lista 2.

---
+++ Lista II/Exercício 5.py
@@ -0,0 +1,10 @@
+number3 = float(input('Número: '))
+number2 = float(input('Número: '))
+number1 = float(input('Número: '))
+
+lista = [number3, number2, number1]
+
+maior = max(lista) 
+menor = min(lista)
+
+print(f'O maior número é: {maior} e o menor é: {menor}')
