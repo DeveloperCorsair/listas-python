906e1794ba1f02018670264b335830fb794136c2
Developer-Martins <henriquemartins7t@gmail.com>
2025-09-06 19:22:30 -0300

    Adição do código para encontrar o maior número entre três entradas do usuário.

---
+++ Lista II/Exercício 4.py
@@ -0,0 +1,8 @@
+number1 = float(input('Número 1: '))
+number2 = float(input('Número 2: '))
+number3 = float(input('Número 3: '))
+lista = [number1, number2, number3]
+
+lista = max(lista)
+
+print(f'O maior entre os números é: {lista}')
