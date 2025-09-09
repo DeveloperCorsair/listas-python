0fa17452701684aedcac0a31064849acd132150f
Henrique <henriquemartins7t@gmail.com>
2025-09-09 09:53:46 -0300

    Feito Exercício 4 para filtrar números com 2 e sem 7

--- Lista V/Exercício 4.py
+++ Lista V/Exercício 4.py
@@ -1,6 +1,6 @@
 #Exercício 4
 
+# Para retornar apenas 2, tirar o segundo for e, vice-versa
 for i in range(18644, 33087):
-    i = str(i)
-
-    print(i)
+    if '2' in str(i) and '7' not in str(i):
+        print('número com 2 e sem 7:',i)
