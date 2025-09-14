2dea14e4e101b7736982456450a839a01ab6ccfa
Developer-Martins <henriquemartins7t@gmail.com>
2025-09-14 00:52:26 -0300

    Feito Exercício a Lista VI

---
+++ Lista VI CodingBat/Exercício a.py
@@ -0,0 +1,14 @@
+# A. dormir
+# dia_semana é True para dias na semana
+# feriado é True nos feriados
+# você pode ficar dormindo quando é feriado ou não é dia semana
+# retorne True ou False conforme você vá dormir ou não
+def dormir(dia_semana, feriado):
+    return not dia_semana or feriado
+    
+    """ if dia_semana True:
+        print('Não é hora de dormir, é dia de semana!')
+    else:
+        print('Você pode dormir. Pois é feriado!') """
+    
+print(dormir(False, False))
