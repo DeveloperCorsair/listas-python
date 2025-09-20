56df1995ff51169ff6e1fe7030241768e10322a2
Developer-Martins <henriquemartins7t@gmail.com>
2025-09-19 21:09:32 -0300

    Exercício F Concluído.

---
+++ Lista VI codingBat/Exercício F.py
@@ -0,0 +1,31 @@
+# F. dez
+# dados dois inteiros a e b
+# retorna True se um dos dois é 10 ou a soma é 10
+def dez(a, b):
+    return a == 10 or b == 10 or a + b == 10
+print(dez(2, 10))
+
+# Provided simple test() function used in main() to print
+# what each function returns vs. what it's supposed to return.
+def test(obtido, esperado):
+  if obtido == esperado:
+    prefixo = ' Parabéns!'
+  else:
+    prefixo = ' Ainda não'
+  print ('%s obtido: %s esperado: %s'
+         % (prefixo, repr(obtido), repr(esperado)))
+
+def main():
+  print ()
+  print ('Dez')
+  test(dez(9, 10), True)
+  test(dez(9, 9), False)
+  test(dez(1, 9), True)
+  test(dez(10, 1), True)
+  test(dez(10, 10), True)
+  test(dez(8, 2), True)
+  test(dez(8, 3), False)
+  test(dez(10, 42), True)
+  test(dez(12, -2), True)
+if __name__ == '__main__':
+  main()
