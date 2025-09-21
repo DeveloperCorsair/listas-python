64f3a22e552cdfc9dbde93cc81a88d08a705a472
Developer-Martins <henriquemartins7t@gmail.com>
2025-09-20 23:01:44 -0300

    Exercício C Concluído.

---
+++ Lista VII codingBat/Exercício B.py
@@ -0,0 +1,32 @@
+# B. string_splosion
+# string_splosion('Code') -> 'CCoCodCode'
+# string_splosion('abc') -> 'aababc'
+# string_splosion('ab') -> 'aab'
+def string_splosion(s):
+  return [s[:-1] + s[0:] for i in range(len(s))]
+
+# Provided simple test() function used in main() to print
+# what each function returns vs. what it's supposed to return.
+def test(obtido, esperado):
+  if obtido == esperado:
+    prefixo = ' Parabéns!'
+  else:
+    prefixo = ' Ainda não'
+  print ('%s obtido: %s esperado: %s'
+         % (prefixo, repr(obtido), repr(esperado)))
+
+def main():
+  print ()
+  print ('String Explosion')
+  test(string_splosion('Code'), 'CCoCodCode')
+  test(string_splosion('abc'), 'aababc')
+  test(string_splosion('ab'), 'aab')
+  test(string_splosion('x'), 'x')
+  test(string_splosion('fade'), 'ffafadfade')
+  test(string_splosion('There'), 'TThTheTherThere')
+  test(string_splosion('Kitten'), 'KKiKitKittKitteKitten')
+  test(string_splosion('Bye'), 'BByBye')
+  test(string_splosion('Good'), 'GGoGooGood')
+  test(string_splosion('Bad'), 'BBaBad')
+if __name__ == '__main__':
+  main()
