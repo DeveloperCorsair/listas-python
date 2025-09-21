3b4647621b39e1e921a00028917b595a11381338
Developer-Martins <henriquemartins7t@gmail.com>
2025-09-20 23:41:37 -0300

    Exercício G Concluído.

---
+++ Lista VII codingBat/Exercício G.py
@@ -0,0 +1,29 @@
+# G. extra_end
+# seja um string s com no mínimo duas letras
+# retorna três vezes as duas últimas letras
+# extra_end('Hello'), 'lololo'
+# extra_end('ab'), 'ababab'
+# extra_end('Hi'), 'HiHiHi'  
+def extra_end(s):
+  return s[-2:] * 3
+
+# Provided simple test() function used in main() to print
+# what each function returns vs. what it's supposed to return.
+def test(obtido, esperado):
+  if obtido == esperado:
+    prefixo = ' Parabéns!'
+  else:
+    prefixo = ' Ainda não'
+  print ('%s obtido: %s esperado: %s'
+         % (prefixo, repr(obtido), repr(esperado)))
+
+def main():
+  print ()
+  print ('Extra End')
+  test(extra_end('Hello'), 'lololo')
+  test(extra_end('ab'), 'ababab')
+  test(extra_end('Hi'), 'HiHiHi')
+  test(extra_end('Candy'), 'dydydy')
+  test(extra_end('Code'), 'dedede')
+if __name__ == '__main__':
+  main()
