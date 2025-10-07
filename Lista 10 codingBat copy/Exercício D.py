5cac477b9c4b39b9093ce7a6af868b1c8af23019
Henrique <henriquemartins7t@gmail.com>
2025-10-07 10:48:47 -0300

    Exercício D Concluído.

---
+++ Lista 10 codingBat copy/Exercício D.py
@@ -0,0 +1,35 @@
+# D. double_char #
+# retorna os caracteres da string original duplicados
+# double_char('The') -> 'TThhee'
+# double_char('AAbb') -> 'AAAAbbbb'
+# double_char('Hi-There') -> 'HHii--TThheerree'
+def double_char(s):
+  o = ''
+  for i in range(len(s)):
+    o += s[i] * 2
+  return o
+
+# Provided simple test() function used in main() to print
+# what each function returns vs. what it's supposed to return.
+def test(obtido, esperado):
+  if obtido == esperado:
+    prefixo = ' Parabéns!'
+  else:
+    prefixo = ' Ainda não'
+  print ('%s obtido: %s esperado: %s'
+         % (prefixo, repr(obtido), repr(esperado)))
+
+def main():
+  print ()
+  print ('Double_char')
+  test(double_char('The'), 'TThhee')
+  test(double_char('AAbb'), 'AAAAbbbb')
+  test(double_char('Hi-There'), 'HHii--TThheerree')
+  test(double_char('Word!'), 'WWoorrdd!!')
+  test(double_char('!!'), '!!!!')
+  test(double_char(''), '')
+  test(double_char('a'), 'aa')
+  test(double_char('.'), '..')
+  test(double_char('aa'), 'aaaa')
+if __name__ == '__main__':
+  main()
