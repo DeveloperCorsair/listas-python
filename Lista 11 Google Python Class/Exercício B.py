6874e5960d44216e7fb40ed0638b8a2eb6ba128c
Henrique <henriquemartins7t@gmail.com>
2025-10-10 07:59:14 -0300

    Exercício B Concluído.

---
+++ Lista 11 Google Python Class/Exercício B.py
@@ -0,0 +1,28 @@
+# B. pontas
+# Dada uma string s, retorna uma string com as duas primeiras e as duas
+# últimas letras da string original s
+# Assim 'palmeiras' retorna 'paas'
+# No entanto, se a string tiver menos que 2 letras, retorna uma string vazia
+def pontas(s):
+  return '' if len(s) < 2 else s[0: 2] + s[-2:]
+
+# Provided simple test() function used in main() to print
+# what each function returns vs. what it's supposed to return.
+def test(obtido, esperado):
+  if obtido == esperado:
+    prefixo = ' Parabéns!'
+  else:
+    prefixo = ' Ainda não'
+  print ('%s obtido: %s esperado: %s'
+         % (prefixo, repr(obtido), repr(esperado)))
+
+
+def main():
+  print ()
+  print ('pontas')
+  test(pontas('palmeiras'), 'paas')
+  test(pontas('algoritmos'), 'alos')
+  test(pontas('a'), '')
+  test(pontas('xyz'), 'xyyz')
+if __name__ == '__main__':
+  main()
