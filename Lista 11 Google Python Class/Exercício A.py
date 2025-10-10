b61789fa547734c20e091fef527c212e8647c1b9
Henrique <henriquemartins7t@gmail.com>
2025-10-10 07:54:45 -0300

    Exercício A da Lista 11 Concluído.

---
+++ Lista 11 Google Python Class/Exercício A.py
@@ -0,0 +1,28 @@
+# A. donuts
+# Para um inteiro n retorna uma string na forma 'Número de donuts: <n>'
+# onde n é o valor passado como argumento.
+# Caso n >= 10 devo retornar 'muitos' em lugar do número.
+# donuts(5) returns 'Número de donuts: 5'
+# donuts(23) returns 'Número de donuts: muitos'
+def donuts(n):
+  return 'Número de donuts: muitos' if n >= 10 else f'Número de donuts: {n}'
+
+# Provided simple test() function used in main() to print
+# what each function returns vs. what it's supposed to return.
+def test(obtido, esperado):
+  if obtido == esperado:
+    prefixo = ' Parabéns!'
+  else:
+    prefixo = ' Ainda não'
+  print ('%s obtido: %s esperado: %s'
+         % (prefixo, repr(obtido), repr(esperado)))
+
+
+def main():
+  print ('donuts')
+  test(donuts(4), 'Número de donuts: 4')
+  test(donuts(9), 'Número de donuts: 9')
+  test(donuts(10), 'Número de donuts: muitos')
+  test(donuts(99), 'Número de donuts: muitos')
+if __name__ == '__main__':
+  main()
