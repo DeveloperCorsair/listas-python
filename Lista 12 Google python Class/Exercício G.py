ad4c3921b2241617efca287edb9bbdc9032e8812
Henrique <henriquemartins7t@gmail.com>
2025-10-14 10:44:01 -0300

    Criação da Lista 12.

---
+++ Lista 12 Google python Class/Exercício G.py
@@ -0,0 +1,27 @@
+# G. verbing
+# Dada uma string, caso seu comprimento seja pelo menos 3,
+# adiciona 'ing' no final
+# Caso a string já termine em 'ing', acrescentará 'ly'.
+# dica use s.endswith('ing')
+def verbing(s):
+  s = str(s)
+  if s[::-1 ]
+  return 
+
+def test(obtido, esperado):
+  if obtido == esperado:
+    prefixo = ' Parabéns!'
+  else:
+    prefixo = ' Ainda não'
+  print ('%s obtido: %s esperado: %s' % (prefixo, repr(obtido), repr(esperado)))
+
+def main():
+  print ('verbing')
+  test(verbing('hail'), 'hailing')
+  test(verbing('swiming'), 'swimingly')
+  test(verbing('do'), 'do')
+if __name__ == '__main__':
+  main()
+
+n = str(n)
+n[::-1]
