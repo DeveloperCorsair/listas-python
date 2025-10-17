a86f49a5727e4e1a4fea4083b0f3a2945b7c6983
Henrique <henriquemartins7t@gmail.com>
2025-10-17 08:54:48 -0300

    primeira parte do exercício H.

---
+++ Lista 12 Google python Class/Exercício H.py
@@ -0,0 +1,30 @@
+# H. not_bad
+# Dada uma string, procura a primeira ocorrência de 'not' e 'bad'
+# Se 'bad' aparece depois de 'not' troca 'not' ... 'bad' por 'good'
+# Assim 'This dinner is not that bad!' retorna 'This dinner is good!'
+# dica use s.find('not') e s.find('bad') para ver
+# as posições 
+def not_bad(s):
+  if s.find('bad') > s.find('not'):
+    return s.replace('not', '') and s.replace('bad', 'good')
+  elif s.find('bad'):
+    return s
+  else:
+    return s
+
+def test(obtido, esperado):
+  if obtido == esperado:
+    prefixo = ' Parabéns!'
+  else:
+    prefixo = ' Ainda não'
+  print ('%s obtido: %s esperado: %s' % (prefixo, repr(obtido), repr(esperado)))
+
+def main():
+  print ()
+  print ('not_bad')
+  test(not_bad('This movie is not so bad'), 'This movie is good')
+  test(not_bad('This dinner is not that bad!'), 'This dinner is good!')
+  test(not_bad('This tea is not hot'), 'This tea is not hot')
+  test(not_bad("It's bad yet not"), "It's bad yet not")
+if __name__ == '__main__':
+  main()
