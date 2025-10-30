f44bf2e9740c41a5eb9e91230cf8782fec9e9b1c
henrique <henriquemartins7t@gmail.com>
2025-10-29 23:38:46 -0300

    Exercício I Primeira parte Concluída.

---
+++ Lista 12 Google python Class/Exercício I.py
@@ -0,0 +1,31 @@
+# I. inicio_final
+# Divida cada string em dois pedaços.
+# Se a string tiver um número ímpar de caracteres
+# o primeiro pedaço terá um caracter a mais,
+# Exemplo: 'abcde', divide-se em 'abc' e 'de'.
+# Dadas 2 strings, a e b, retorna a string
+# a_inicio + b_inicio + a_final + b_final
+def inicio_final(a, b):
+  initial_a = (len(a) + 1) // 2
+  initial_b = (len(b) + 1) // 2
+
+  if len(a) % 2 == 1:
+    return a[:initial_a] + b[:initial_b] + a[initial_a:] + b[initial_b:]
+  else: 
+    False
+
+def test(obtido, esperado):
+  if obtido == esperado:
+    prefixo = ' Parabéns!'
+  else:
+    prefixo = ' Ainda não'
+  print ('%s obtido: %s esperado: %s' % (prefixo, repr(obtido), repr(esperado)))
+
+def main():
+  print ()
+  print ('inicio_final')
+  test(inicio_final('abcd', 'xy'), 'abxcdy')
+  test(inicio_final('abcde', 'xyz'), 'abcxydez')
+  test(inicio_final('Kitten', 'Donut'), 'KitDontenut')
+if __name__ == '__main__':
+  main()
