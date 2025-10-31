100ef2c3c243d24cd6d0e4c143fe4c0c3a4d296c
henrique <henriquemartins7t@gmail.com>
2025-10-31 10:13:14 -0300

    Exercício K Concluído.

--- Lista 12 Google python Class/Exercício K.py
+++ Lista 12 Google python Class/Exercício K.py
@@ -2,13 +2,16 @@
 # Verifique quantas vezes o dígito 2 aparece entre 0 e n-1
 # Exemplo: para n = 20 o dígito 2 aparece duas vezes entre 0 e 19
 def conta2(n):
+  contador = 0
   for i in range(0, n-1):
-    n = str(n)
-    n.count('2')
-  return n.count('2')
+    i = str(i)
+    if '2' in i:      
+      a = i.count('2')
+      contador = contador + a
+  return contador
 
 def test(obtido, esperado):
-  if obtido == esperado:
+  if obtido == esperado:''
     prefixo = ' Parabéns!'
   else:
     prefixo = ' Ainda não'
