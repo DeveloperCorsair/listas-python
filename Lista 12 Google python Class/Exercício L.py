187981e1900251a2ad072753218ab11abec4217f
henrique <henriquemartins7t@gmail.com>
2025-10-31 11:02:19 -0300

    Exercício L Concluído.

---
+++ Lista 12 Google python Class/Exercício L.py
@@ -0,0 +1,28 @@
+# L. inicio em potencia de 2
+# Dado um número inteiro positivo n retorne a primeira potência de 2
+# que tenha o início igual a n
+# Exemplo: para n = 65 retornará 16 pois 2**16 = 65536
+# dica transforme para texto a potencia de 2
+# e use .starswith(str(n))
+def inip2(n):
+  contador = 0
+  while True:
+    contador += 1
+    if str(2**contador).startswith(str(n)):
+      return contador
+
+def test(obtido, esperado):
+  if obtido == esperado:
+    prefixo = ' Parabéns!'
+  else:
+    prefixo = ' Ainda não'
+  print ('%s obtido: %s esperado: %s' % (prefixo, repr(obtido), repr(esperado)))
+
+def main():
+  print ()
+  print ('inicio p2')
+  test(inip2(7), 46)
+  test(inip2(133), 316)
+  test(inip2(1024), 10)
+if __name__ == '__main__':
+  main()
